328a10132db89689d8a445d21a29c2b45c75170f
topalyh <alex.k.topal@gmail.com>
2025-10-03 14:58:36 +0300

    DeepScope.lua

--- DeepScope.lua
+++ DeepScope.lua
@@ -3478,7 +3478,6 @@ local function createGui()
 		ZIndex = 2,
 		FontFace = Font.new("rbxassetid://16658246179"),
 		Text = [[print("Hello DeepScope!")]],
-		TextTransparency = 1,
 		TextColor3 = Color3.fromRGB(204, 204, 204),
 		TextSize = 15,
 		RichText = true,
@@ -3643,7 +3642,7 @@ local function createGui()
 		Parent = executorGui16,
 		PaddingRight = UDim.new(0, 35)
 	})
-	executorGui3.Text = modules.other.executor.highlightLuau(executorGui3.ContentText)
+	executorGui3.Text = modules.other.executor.highlightLuau(executorGui3_2.ContentText)
 	modules.other.executor.Update(executorGui3.Text, executorGui3, executorGui5)
 	infoList = placeInfoGui4
 	logList = logGui2
@@ -5779,3 +5778,4 @@ while true do
 		newgui.spawndistance.Text = "distance from spawn: unknown | unknown"
 	end
 end
+
