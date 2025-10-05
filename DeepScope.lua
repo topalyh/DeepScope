9fb130e8e2b08b0f436e442acabe86460f753bc4
topalyh <alex.k.topal@gmail.com>
2025-10-05 22:46:16 +0300

    DeepScope.lua

--- DeepScope.lua
+++ DeepScope.lua
@@ -89,10 +89,214 @@ local fonts = {
 	["FiraSans"] = "rbxassetid://12187374954"
 }
 local icons = {
-	size = {19, 19},
+	size = {16, 16},
 	UIsize = {19, 19},
-	icons = {
-		
+	index = {
+		["Accessory"] = 32;
+		["Accoutrement"] = 32;
+		["AdService"] = 73;
+		["Animation"] = 60;
+		["AnimationController"] = 60;
+		["AnimationTrack"] = 60;
+		["Animator"] = 60;
+		["ArcHandles"] = 56;
+		["AssetService"] = 72;
+		["Attachment"] = 34;
+		["Backpack"] = 20;
+		["BadgeService"] = 75;
+		["BallSocketConstraint"] = 89;
+		["BillboardGui"] = 64;
+		["BinaryStringValue"] = 4;
+		["BindableEvent"] = 67;
+		["BindableFunction"] = 66;
+		["BlockMesh"] = 8;
+		["BloomEffect"] = 90;
+		["BlurEffect"] = 90;
+		["BodyAngularVelocity"] = 14;
+		["BodyForce"] = 14;
+		["BodyGyro"] = 14;
+		["BodyPosition"] = 14;
+		["BodyThrust"] = 14;
+		["BodyVelocity"] = 14;
+		["BoolValue"] = 4;
+		["BoxHandleAdornment"] = 54;
+		["BrickColorValue"] = 4;
+		["Camera"] = 5;
+		["CFrameValue"] = 4;
+		["CharacterMesh"] = 60;
+		["Chat"] = 33;
+		["ClickDetector"] = 41;
+		["CollectionService"] = 30;
+		["Color3Value"] = 4;
+		["ColorCorrectionEffect"] = 90;
+		["ConeHandleAdornment"] = 54;
+		["Configuration"] = 58;
+		["ContentProvider"] = 72;
+		["ContextActionService"] = 41;
+		["CoreGui"] = 46;
+		["CoreScript"] = 18;
+		["CornerWedgePart"] = 1;
+		["CustomEvent"] = 4;
+		["CustomEventReceiver"] = 4;
+		["CylinderHandleAdornment"] = 54;
+		["CylinderMesh"] = 8;
+		["CylindricalConstraint"] = 89;
+		["Debris"] = 30;
+		["Decal"] = 7;
+		["Dialog"] = 62;
+		["DialogChoice"] = 63;
+		["DoubleConstrainedValue"] = 4;
+		["Explosion"] = 36;
+		["FileMesh"] = 8;
+		["Fire"] = 61;
+		["Flag"] = 38;
+		["FlagStand"] = 39;
+		["FloorWire"] = 4;
+		["Folder"] = 70;
+		["ForceField"] = 37;
+		["Frame"] = 48;
+		["GamePassService"] = 19;
+		["Glue"] = 34;
+		["GuiButton"] = 52;
+		["GuiMain"] = 47;
+		["GuiService"] = 47;
+		["Handles"] = 53;
+		["HapticService"] = 84;
+		["Hat"] = 45;
+		["HingeConstraint"] = 89;
+		["Hint"] = 33;
+		["HopperBin"] = 22;
+		["HttpService"] = 76;
+		["Humanoid"] = 9;
+		["ImageButton"] = 52;
+		["ImageLabel"] = 49;
+		["InsertService"] = 72;
+		["IntConstrainedValue"] = 4;
+		["IntValue"] = 4;
+		["JointInstance"] = 34;
+		["JointsService"] = 34;
+		["Keyframe"] = 60;
+		["KeyframeSequence"] = 60;
+		["KeyframeSequenceProvider"] = 60;
+		["Lighting"] = 13;
+		["LineHandleAdornment"] = 54;
+		["LocalScript"] = 18;
+		["LogService"] = 87;
+		["MarketplaceService"] = 46;
+		["Message"] = 33;
+		["Model"] = 2;
+		["ModuleScript"] = 71;
+		["Motor"] = 34;
+		["Motor6D"] = 34;
+		["MoveToConstraint"] = 89;
+		["NegateOperation"] = 78;
+		["NetworkClient"] = 16;
+		["NetworkReplicator"] = 29;
+		["NetworkServer"] = 15;
+		["NumberValue"] = 4;
+		["ObjectValue"] = 4;
+		["Pants"] = 44;
+		["ParallelRampPart"] = 1;
+		["Part"] = 1;
+		["ParticleEmitter"] = 69;
+		["PartPairLasso"] = 57;
+		["PathfindingService"] = 37;
+		["Platform"] = 35;
+		["Player"] = 12;
+		["PlayerGui"] = 46;
+		["Players"] = 21;
+		["PlayerScripts"] = 82;
+		["PointLight"] = 13;
+		["PointsService"] = 83;
+		["Pose"] = 60;
+		["PrismaticConstraint"] = 89;
+		["PrismPart"] = 1;
+		["PyramidPart"] = 1;
+		["RayValue"] = 4;
+		["ReflectionMetadata"] = 86;
+		["ReflectionMetadataCallbacks"] = 86;
+		["ReflectionMetadataClass"] = 86;
+		["ReflectionMetadataClasses"] = 86;
+		["ReflectionMetadataEnum"] = 86;
+		["ReflectionMetadataEnumItem"] = 86;
+		["ReflectionMetadataEnums"] = 86;
+		["ReflectionMetadataEvents"] = 86;
+		["ReflectionMetadataFunctions"] = 86;
+		["ReflectionMetadataMember"] = 86;
+		["ReflectionMetadataProperties"] = 86;
+		["ReflectionMetadataYieldFunctions"] = 86;
+		["RemoteEvent"] = 80;
+		["RemoteFunction"] = 79;
+		["ReplicatedFirst"] = 72;
+		["ReplicatedStorage"] = 72;
+		["RightAngleRampPart"] = 1;
+		["RocketPropulsion"] = 14;
+		["RodConstraint"] = 89;
+		["RopeConstraint"] = 89;
+		["Rotate"] = 34;
+		["RotateP"] = 34;
+		["RotateV"] = 34;
+		["RunService"] = 66;
+		["ScreenGui"] = 47;
+		["Script"] = 6;
+		["ScrollingFrame"] = 48;
+		["Seat"] = 35;
+		["Selection"] = 55;
+		["SelectionBox"] = 54;
+		["SelectionPartLasso"] = 57;
+		["SelectionPointLasso"] = 57;
+		["SelectionSphere"] = 54;
+		["ServerScriptService"] = 0;
+		["ServerStorage"] = 74;
+		["Shirt"] = 43;
+		["ShirtGraphic"] = 40;
+		["SkateboardPlatform"] = 35;
+		["Sky"] = 28;
+		["SlidingBallConstraint"] = 89;
+		["Smoke"] = 59;
+		["Snap"] = 34;
+		["Sound"] = 11;
+		["SoundService"] = 31;
+		["Sparkles"] = 42;
+		["SpawnLocation"] = 25;
+		["SpecialMesh"] = 8;
+		["SphereHandleAdornment"] = 54;
+		["SpotLight"] = 13;
+		["SpringConstraint"] = 89;
+		["StarterCharacterScripts"] = 82;
+		["StarterGear"] = 20;
+		["StarterGui"] = 46;
+		["StarterPack"] = 20;
+		["StarterPlayer"] = 88;
+		["StarterPlayerScripts"] = 82;
+		["Status"] = 2;
+		["StringValue"] = 4;
+		["SunRaysEffect"] = 90;
+		["SurfaceGui"] = 64;
+		["SurfaceLight"] = 13;
+		["SurfaceSelection"] = 55;
+		["Team"] = 24;
+		["Teams"] = 23;
+		["TeleportService"] = 81;
+		["Terrain"] = 65;
+		["TerrainRegion"] = 65;
+		["TestService"] = 68;
+		["TextBox"] = 51;
+		["TextButton"] = 51;
+		["TextLabel"] = 50;
+		["Texture"] = 10;
+		["TextureTrail"] = 4;
+		["Tool"] = 17;
+		["TouchTransmitter"] = 37;
+		["TrussPart"] = 1;
+		["UnionOperation"] = 77;
+		["UserInputService"] = 84;
+		["Vector3Value"] = 4;
+		["VehicleSeat"] = 35;
+		["VelocityMotor"] = 34;
+		["WedgePart"] = 1;
+		["Weld"] = 34;
+		["Workspace"] = 19;
 	},
 	UIicons = {
 		dropdown_closed = {199.5, 199.5},
@@ -3361,10 +3565,9 @@ local function createEntryForInstance(node, parentGui)
 		BackgroundTransparency = 1,
 		LayoutOrder = 1,
 		Size = UDim2.fromOffset(17, 17),
-		Image = "rbxassetid://765660635",
+		Image = imageId,
 		BorderColor3 = Color3.new(0, 0, 0),
 		BorderSizePixel = 0,
-		ImageRectOffset = Vector2.new(1.5, 1.5),
 		ImageRectSize = Vector2.new(table.unpack(icons.size))
 	})
 	local explorerGui12 = createInstance("TextLabel", {
@@ -3421,9 +3624,8 @@ local function createEntryForInstance(node, parentGui)
 	newTemplate.Parent = parentGui
 	newTemplate.Name = node.Data.Name
 	newTemplate.mainframe.name.Text = node.Data.Name
-	local iconCoords = icons.icons[node.Data.ClassName] or icons.icons.Unknown
-	newTemplate.mainframe.icon.ImageRectOffset = Vector2.new(iconCoords[1], iconCoords[2])
-	newTemplate.mainframe.icon.ImageRectSize = Vector2.new(table.unpack(icons.size))
+	local index = icons.index[node.Data.ClassName] or 0
+	newTemplate.mainframe.icon.ImageRectOffset = Vector2.new(icons.size[1]*index, 0)
 	guiToNode[newTemplate] = node
 	nodeToGui[node] = newTemplate
 
@@ -3457,7 +3659,7 @@ end
 
 local function buildChildrenNodes(instance, parentNode, parentGui)
 	local node = {
-		Instance = instance, -- ïðèâÿçêà íàïðÿìóþ!
+		Instance = instance, -- привязка напрямую!
 		Data = {
 			Name = instance.Name,
 			ClassName = instance.ClassName,
@@ -3466,29 +3668,29 @@ local function buildChildrenNodes(instance, parentNode, parentGui)
 		Children = {}
 	}
 
-	-- ñîçäà¸ì PropertiesData
+	-- создаём PropertiesData
 	nodeToProps[node] = makeProperties(instance)
 
-	-- UI ýëåìåíò
+	-- UI элемент
 	local entry = createEntryForInstance(node, parentGui)
 	guiToNode[entry] = node
 	nodeToGui[node] = entry
 
-	-- ñëóøàåì èçìåíåíèÿ Name/Parent (ïåðåèìåíîâàíèÿ è ïåðåìåùåíèÿ)
+	-- слушаем изменения Name/Parent (переименования и перемещения)
 	attachPropertyListeners(instance, node)
 
-	-- ñëóøàåì ïîÿâëåíèå äåòåé
+	-- слушаем появление детей
 	instance.ChildAdded:Connect(function(child)
 		local childNode = buildChildrenNodes(child, node, entry.dropdown)
 		table.insert(node.Children, childNode)
 		recalcAndPropagateSize(entry)
 	end)
 
-	-- ñëóøàåì óäàëåíèå äåòåé
+	-- слушаем удаление детей
 	instance.ChildRemoved:Connect(function(child)
 		for i, childNode in ipairs(node.Children) do
-			if childNode.Instance == child then -- ñðàâíåíèå ïî îáúåêòó!
-				-- óäàëÿåì UI
+			if childNode.Instance == child then -- сравнение по объекту!
+				-- удаляем UI
 				local gui = nodeToGui[childNode]
 				if gui then
 					gui:Destroy()
@@ -3496,7 +3698,7 @@ local function buildChildrenNodes(instance, parentNode, parentGui)
 				end
 				nodeToGui[childNode] = nil
 
-				-- óäàëÿåì Properties
+				-- удаляем Properties
 				nodeToProps[childNode] = nil
 
 				table.remove(node.Children, i)
@@ -3506,7 +3708,7 @@ local function buildChildrenNodes(instance, parentNode, parentGui)
 		end
 	end)
 
-	-- ðåêóðñèâíî ñîçäà¸ì äî÷åðíèå íîäû
+	-- рекурсивно создаём дочерние ноды
 	for _, child in ipairs(instance:GetChildren()) do
 		local childNode = buildChildrenNodes(child, node, entry.dropdown)
 		table.insert(node.Children, childNode)
